084a6e6d999e94e6e3e7fbd0305d79e8429359d1
vansh101121 <vansh101121@gmail.com>
2026-07-08 20:47:40 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Enable CORS for frontend development
 app.use(
   cors({
-    origin: ["http://0.0.0.0/0:5173"],
+    origin: ["https://cardeckapi.vercel.app"],
     credentials: true,
   }),
 );
